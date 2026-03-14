dbc7cc0e859c5d45681375f6f6707752aabfb686
mauro junior <125211102+mj01px@users.noreply.github.com>
2026-03-14 00:13:17 -0300

    feat: register Game and GameKey models in admin panel

---
+++ game-rent-api/games/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Game, GameKey
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ['name', 'platform', 'rental_price', 'rating', 'is_featured']
+    list_filter = ['platform', 'is_featured']
+    search_fields = ['name']
+
+
+@admin.register(GameKey)
+class GameKeyAdmin(admin.ModelAdmin):
+    list_display = ['game', 'key', 'status']
+    list_filter = ['status']
